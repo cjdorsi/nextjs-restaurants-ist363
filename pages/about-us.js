a320458c29f70992caa1c9485450453b7df1b7cf
cjdorsi <cjdorsi@syr.edu>
2022-12-19 19:43:55 -0500

    trying again

--- pages/about-us.js
+++
@@ -1,9 +0,0 @@
-import Layout from "../components/Layout"
-
-const AboutUsPage = () => {
-    return <Layout>
-        <h1>About Us</h1>
-        <p>Paragraph about the company.</p>
-    </Layout>
-}
-export default AboutUsPage
